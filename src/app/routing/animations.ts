accced1cdef492adcd443ac7d05f44fb59e984be
Guus Ekkelenkamp <ekkelenkampguus@gmail.com>
2018-10-26 12:59:26 +0200

    Improved consistency between animations.

--- src/app/routing/animations.ts
+++ src/app/routing/animations.ts
@@ -7,11 +7,11 @@ export const routerTransition = trigger('routerTransition', [
         group([
             query(':enter', [
                 style({transform: 'translateX(100%)'}),
-                animate('0.5s ease-in-out', style({transform: 'translateX(0%)'}))
+                animate('0.3s ease-in-out', style({transform: 'translateX(0%)'}))
             ], {optional: true}),
             query(':leave', [
                 style({transform: 'translateX(0%)'}),
-                animate('0.5s ease-in-out', style({transform: 'translateX(-100%)'}))
+                animate('0.3s ease-in-out', style({transform: 'translateX(-100%)'}))
             ], {optional: true}),
         ])
     ]),
