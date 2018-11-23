b1040b890110c710d2702ea64b6db9dc903f2385
Guus Ekkelenkamp <ekkelenkampguus@gmail.com>
2018-11-23 11:37:37 +0100

    Changed structure of competentie component

--- src/app/pages/competentie/competentie.component.ts
+++ src/app/pages/competentie/competentie.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { from } from 'rxjs';
 import { PrismicService } from '../../services/prismic.service';
 import PrismicDOM from 'prismic-dom';
 import { Location } from '@angular/common';
+import { ProjectUtils } from '../../app.utils';
+import { SafeHtml } from '@angular/platform-browser';
 
 @Component({
     selector: 'app-competentie',
@@ -12,13 +14,21 @@ import { Location } from '@angular/common';
 })
 
 export class CompetentieComponent implements OnInit {
-    content = Object;
-    PrismicDOM = PrismicDOM;
+    private _body: any;
+    private _title: string;
+    private _category: string;
+    private _competenties: {
+        title: string;
+        desc: SafeHtml;
+    };
+    private _slices: {
+        slice: Object;
+        type: string
+    }[];
 
     constructor(
         private route: ActivatedRoute,
         private prismicService: PrismicService,
-        private _location: Location,
     ) {
     }
 
@@ -33,7 +43,52 @@ export class CompetentieComponent implements OnInit {
 
     getDocument(uid: string): void {
         from(this.prismicService.getDocument(uid))
-            .subscribe(content => this.content = content['value']);
+            .subscribe((document) => {
+                this._body = ProjectUtils.childObjectBySelector(document['value'], 'data/body/1', null);
+                this._title = ProjectUtils.childObjectBySelector(document['value'], 'data/title/0/text', null);
+                this._category = ProjectUtils.childObjectBySelector(document['value'], 'data/competentie_cat/0/text', null);
+                this._competenties = this.generateCompetenties(ProjectUtils.childObjectBySelector(document['value'], 'data/competenties', []));
+                this._slices = this.generateSlices(ProjectUtils.childObjectBySelector(document['value'], 'data/body', []));
+            });
+    }
+
+    private generateCompetenties(data) {
+        return data.map((competentie) => {
+            return {
+                title: ProjectUtils.childObjectBySelector(competentie, 'comp_titel/0/text', []),
+                desc: this.prismicService.toHtml(ProjectUtils.childObjectBySelector(competentie, 'comp_desc', [])),
+            };
+        });
+    }
+
+    private generateSlices(data) {
+        return data.map((slice) => {
+            return {
+                slice: slice,
+                type: slice.slice_type
+            };
+        });
+    }
+
+    get category(): string {
+        return this._category;
+    }
+
+    get body(): any {
+        return this._body;
+    }
+
+    get competenties(): { title: string; desc: SafeHtml } {
+        return this._competenties;
+    }
+
+    get title(): string {
+        return this._title;
+    }
+
+
+    get slices(): { slice: Object; type: string }[] {
+        return this._slices;
     }
 
     public replace(content: string) {
