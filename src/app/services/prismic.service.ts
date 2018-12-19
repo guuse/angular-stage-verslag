484bf5a62453409737ecd59e30c14bffd6502a39
Guus Ekkelenkamp <ekkelenkampguus@gmail.com>
2018-12-19 11:12:07 +0100

    Add linkresolver for linking to competenties.

--- src/app/services/prismic.service.ts
+++ src/app/services/prismic.service.ts
@@ -5,9 +5,13 @@ import * as Prismic from 'prismic-javascript';
 import * as PrismicDOM from 'prismic-dom';
 import {CONFIG} from '../../prismic-configuration';
 import { DomSanitizer } from '@angular/platform-browser';
+import { ProjectUtils } from '../app.utils';
 
 @Injectable({providedIn: 'root'})
 export class PrismicService {
+    private _docMap = {
+        'content': '#/competenties/'
+    };
 
     constructor(
         private sanitizer: DomSanitizer,
@@ -41,7 +45,15 @@ export class PrismicService {
     }
 
     public toHtml(data) {
-        return this.sanitizer.bypassSecurityTrustHtml(PrismicDOM.RichText.asHtml(data));
+        return this.sanitizer.bypassSecurityTrustHtml(PrismicDOM.RichText.asHtml(data, this.linkResolver.bind(this)));
+    }
+
+    public linkResolver(doc) {
+        if (doc.uid != null && doc.type != null) {
+            const baseUrl = ProjectUtils.childObjectBySelector(this._docMap, doc.type, null);
+            return baseUrl ? baseUrl + doc.uid : null;
+        }
+        return null;
     }
 
     public toHtmlVideo(data) {
